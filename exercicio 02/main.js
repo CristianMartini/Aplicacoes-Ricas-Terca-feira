94f933ad3b601d3f208ead3306e56125830bd14a
Cristian martini <cristianmartini.sp@gmail.com>
2023-08-22 22:24:01 -0300

    aula 2

---
+++ exercicio 02/main.js
@@ -0,0 +1,14 @@
+const worker = new Worker('worker.js');
+
+worker.addEventListener('message', function(event) {
+  const square = event.data;
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = 'O quadrado do número é: ' + square;
+});
+function calculateSquare() {
+  const inputElement = document.getElementById('inputNumber');
+  const inputNumber = Number(inputElement.value);
+  worker.postMessage(inputNumber);
+}
+  
+  
